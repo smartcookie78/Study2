f244a3eda6fb58d80dbbfe50bcad28616438ac27
Pavel Kuzmenko <pavlikuz@gmail.com>
2023-11-19 20:50:07 +0200

    Oprimized version

--- Program.cs
+++ Program.cs
@@ -24,44 +24,45 @@ namespace Study2
             Console.WriteLine("2 - USD to Rub?");
             answer = Console.ReadLine();
 
-            if (answer == "1")
+            switch(answer)
             {
-                Console.WriteLine("Выбрана операция Rub to USD");
-                Console.WriteLine("Сколько рублей вы хотите конвертировать?");
-                numberOfRub = Convert.ToSingle(Console.ReadLine());
-
-                if (numberOfRub > totalRub) 
-                {
-                    Console.WriteLine("Введено неверное количество рублей");                   
-                }
-                else
-                {
-                    totalUsd = numberOfRub / rubToUsd;
-                    totalRub -= numberOfRub;
-                    Console.WriteLine($"Ваш баланс {Math.Round(totalUsd, 2)} долларов и {totalRub} рублей");
-                    Console.WriteLine("Спасибо за пользование нашим сервисом!");
-                }
-
+                case "1":
+                    Console.WriteLine("Выбрана операция Rub to USD");
+                    Console.WriteLine("Сколько рублей вы хотите конвертировать?");
+                    numberOfRub = Convert.ToSingle(Console.ReadLine());
+                    if (numberOfRub > totalRub)
+                    {
+                        Console.WriteLine("Введено неверное количество рублей");
+                    }
+                    else
+                    {
+                        totalUsd = numberOfRub / rubToUsd;
+                        totalRub -= numberOfRub;
+                        Console.WriteLine($"Ваш баланс {Math.Round(totalUsd, 2)} долларов и {totalRub} рублей");
+                        Console.WriteLine("Спасибо за пользование нашим сервисом!");
+                    }
+                    break;
+                case "2":
+                    Console.WriteLine("Выбрана операция UDS to RUB");
+                    Console.WriteLine("Сколько долларов вы хотите конвертировать?");
+                    numberOfUsd = Convert.ToSingle(Console.ReadLine());
+                    if (numberOfUsd > totalUsd)
+                    {
+                        Console.WriteLine("У Вас недостаточно долларов для конвертации");
+                    }
+                    else
+                    {
+                        totalRub = numberOfUsd * usdToRub;
+                        totalUsd -= numberOfUsd;
+                        Console.WriteLine($"Ваш баланс {Math.Round(totalUsd, 2)} долларов и {Math.Round(totalRub, 2)} рублей");
+                        Console.WriteLine("Спасибо за пользование нашим сервисом!");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Неверная операция");
+                    break;
             }
-            else if (answer == "2")
-            {
-                Console.WriteLine("Выбрана операция UDS to RUB");
-                Console.WriteLine("Сколько долларов вы хотите конвертировать?");
-                numberOfUsd = Convert.ToSingle(Console.ReadLine());
-                if (numberOfUsd >  totalUsd)
-                {
-                    Console.WriteLine("У Вас недостаточно долларов для конвертации");
-                }
-                else
-                { 
-                    totalRub = numberOfUsd * usdToRub;
-                    totalUsd -= numberOfUsd;
-                    Console.WriteLine($"Ваш баланс {Math.Round(totalUsd, 2)} долларов и {Math.Round(totalRub, 2)} рублей");
-                    Console.WriteLine("Спасибо за пользование нашим сервисом!");
-                }
 
-               
-            }
         Console.ReadKey();
 
 
